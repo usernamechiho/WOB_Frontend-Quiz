86c1609c0943196cd6b77b145363aad88d59dd38
Chiho <minepanorama@naver.com>
2022-06-05 04:00:44 +0900

    MOD: GNB 수정

--- src/GNB/index.tsx
+++ src/GNB/index.tsx
@@ -1,9 +1,23 @@
 import styles from './header.module.scss'
 
+import { useSelector } from 'react-redux'
+import { getIsStart, getStage } from 'states/result'
+
 const Header = () => {
+  const isStart = useSelector(getIsStart)
+  const currentStage = useSelector(getStage)
+
   return (
-    <header className={styles.header}>
-      <div>hello</div>
+    <header>
+      <nav className={styles.navContainer}>
+        <h1>Frontend Quiz</h1>
+        {isStart && (
+          <>
+            <p>{currentStage} / 10</p>
+            <button type='button'>Quit</button>
+          </>
+        )}
+      </nav>
     </header>
   )
 }
