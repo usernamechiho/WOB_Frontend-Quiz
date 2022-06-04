719d8e95b75afc63070da374bdad17af555ce428
Chiho <minepanorama@naver.com>
2022-06-05 07:03:21 +0900

    ADD: 결과 페이지 차트 추가

--- src/routes/ResultPage/index.tsx
+++ src/routes/ResultPage/index.tsx
@@ -1,6 +1,9 @@
+import styles from './resultPage.module.scss'
 import { useMount, useUnmount } from 'hooks'
 import { useDispatch, useSelector } from 'react-redux'
 import { getScore, setQuizReset, setTotalScoreReset } from 'states/result'
+
+import { VictoryPie } from 'victory'
 
 const ResultPage = () => {
   const dispatch = useDispatch()
@@ -10,13 +13,23 @@ const ResultPage = () => {
     dispatch(setQuizReset())
   })
 
-  useUnmount(() => {
-    dispatch(setTotalScoreReset())
-  })
-
   console.log(totalScore)
 
-  return <div>{totalScore}</div>
+  return (
+    <div className={styles.resultPageContainer}>
+      <div className={styles.scoreChartContainer}>
+        <VictoryPie
+          data={[
+            { x: 'score', y: totalScore },
+            { x: 'percent', y: 100 - totalScore },
+          ]}
+          style={{ labels: { fontSize: 20 } }}
+        />
+        <p>{totalScore} %</p>
+      </div>
+      <button type='button'>Check Wrong Answers</button>
+    </div>
+  )
 }
 
 export default ResultPage
