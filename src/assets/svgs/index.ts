19e8c2ebe7a4314a5eba660c83df2e4296446a9c
Chiho <minepanorama@naver.com>
2022-06-11 23:30:23 +0900

    ADD: SVG파일 세개 추가 (정답, 오답, 문제)

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -1,2 +1,6 @@
 export { ReactComponent as JavascriptImage } from './javascript.svg'
 export { ReactComponent as ReactImage } from './react.svg'
+
+export { ReactComponent as Correct } from './correct.svg'
+export { ReactComponent as Wrong } from './wrong.svg'
+export { ReactComponent as Normal } from './normal.svg'
