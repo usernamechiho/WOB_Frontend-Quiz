49f510d66c1c159235f0c6541e4fd60344521539
Chiho <minepanorama@naver.com>
2022-06-13 00:18:01 +0900

    MOD: SVG 파일 아이콘 수정

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -2,5 +2,5 @@ export { ReactComponent as JavascriptImage } from './javascript.svg'
 export { ReactComponent as ReactImage } from './react.svg'
 
 export { ReactComponent as Correct } from './correct.svg'
-export { ReactComponent as Wrong } from './wrong.svg'
+export { ReactComponent as Incorrect } from './incorrect.svg'
 export { ReactComponent as Normal } from './normal.svg'
